f2928da503fdc75df33936317c76b9757b46bf93
Yudi810 <contact.wahyudiarfandi@gmail.com>
2026-03-15 13:53:29 +0800

    improved timer accuracy

--- frontend/src/routes/reading/+page.svelte
+++ frontend/src/routes/reading/+page.svelte
@@ -1,16 +1,26 @@
 <script>
     let time = $state(0);
-    let interval = $state()
+    let interval = $state();
+    let startTimestamp;
 
     const startTime = ()=>{
         clearInterval(interval)
+        startTimestamp = Date.now() // Date.now() instead of time++ to prevent drifting
         interval = setInterval(() => {
-        time++
-        }, 1000)
+            // flooring doesn't affect acurracy at all in this case; it's only there to hide decimals
+            time = Math.floor((Date.now() - startTimestamp)/ 100) 
+        }, 100)
     }
+    let show = $state(false) 
+    let text = $state("この世の中には「陰謀」が存在する")
+    let cpm =  $derived((text.length / time)*600)  // 60 for s, 600 for ds, 60000 for ms
 </script>
 
-<button onclick={startTime}>start</button>
-<button onclick={()=> clearInterval(interval)}>pause</button>
-<button onclick={()=> time = 0}>reset</button>
+<button onclick={()=> {startTime(); show = false}}>start</button>
+<button onclick={()=> {clearInterval(interval); show = true}}>stop</button>
 <h1>{time}</h1>
+<p>{text}</p>
+{#if show && time > 0}
+<p>your cpm is: {Math.round(cpm)}</p>
+<button onclick={()=> time = 0}>reset</button>
+{/if}
