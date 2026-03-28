fea534f1c575351c84321128f131d4916d3091f1
Yudi810 <contact.wahyudiarfandi@gmail.com>
2026-03-28 16:27:21 +0800

    improved dict lookup (more refined, less noise)

--- japanese.py
+++ japanese.py
@@ -4,7 +4,6 @@ import uvicorn
 from database import init_db, get_db
 import spacy
 import json
-import ginza
 import jaconv
 
 app = FastAPI()
@@ -30,55 +29,84 @@ def homepage():
     return chars
 
 
+def convert_to_sets(reasons_data):
+    converted = []
+    for reason, rules in reasons_data.items():
+        variants = []
+        for rule in rules:
+            # convert to set first thing so we can check rule overlaps with `&` operation later
+            variants.append((
+                rule["kanaIn"],
+                rule["kanaOut"],
+                set(rule["rulesIn"]), 
+                set(rule["rulesOut"])
+            ))
+        converted.append((reason, variants))
+        # format example
+        # ("past", [
+        #   ("った", "う", {'v5'}, set()),
+        #   "た", "る", {'v1'}, set()),
+        #   etc
+        #  ])
+    return converted
+
 with open("assets/deinflection.json", "r", encoding="utf-8") as f:
-    deinflect = json.loads(f.read())
+    deinflect_raw = json.loads(f.read())
+    deinflect_conv = convert_to_sets(deinflect_raw)
+
+def deinflect_word(source):
+    results = [{"word": source, "rules": set(), "reasons": []}]
+    i = 0
+    # `while` instead of `for` because the latter only captures len() at the start of the loop, while the former checks it every iteration
+    # and since len of results is dynamic with the append at every iteration (except when it has no more to append)
+    while i < len(results): 
+        item = results[i]
+        for reason, variants in deinflect_conv: # first and multi-step deinflections combined
+            for kana_in, kana_out, rules_in, rules_out in variants:
+                # checks if two overlapping rules exist to filter out rules_in that don't match (even if reason is the exact same)
+                # 例: adj-i (item rules) can only match with adj-i (rules in) and so on
+                # also combining these 3 cond into if true might be spaghetti so i'll leave them be for now  
+                if item["rules"] and (not item["rules"] & rules_in): 
+                    continue
+                if not item["word"].endswith(kana_in):
+                    continue
+                if len(item["word"]) - len(kana_in) + len(kana_out) <= 0:
+                    continue
+                new_word = item["word"][:-len(kana_in)] + kana_out
+                results.append({
+                    "word": new_word,
+                    "rules": rules_out,
+                    "reasons": [reason] + item["reasons"]
+                })
+        i += 1
+    return results
+
 
 # TODO: restore rules, look for raw word first, if none then deiflect (label them with their rule also). query with said rule. 
 @app.get("/api/lookup")
-def lookup(word: str, dict: str = "en") -> list:    # "en" is the default arg
-    conn = get_db()                                 # also apparently lists, string, or anything as simple only requires queries instead of req body?
+def lookup(word: str, dict: str = "en") -> list:
+    #if dict not in ["endict", "jpdict"]: # this should prevent sql injections (hopefully)
+    #    return ["ないです"] TODO: implement these later, or not
+    conn = get_db()                                 
     cursor = conn.cursor()
-    #if dict not in ["endict", "jpdict"]:
-    #    return ["no"]
-    pairs = [] # a list of tuples of (original, deinflect) to validate later
-    candidates = []
     deinflects = []
-    all_rules = [rule for rules in deinflect.values() for rule in rules] # deinflect = {category : rules}
-    # if not all('\u4e00' <= c <= '\u9fff' for c in word[0:4]): # checks if yojijukugo, if no use nlp
-    #     word_pre = nlp(word)
-    #     words = [span for span in ginza.bunsetu_spans(word_pre)]
-    #     print(words)
-    #     word = words[0].text
-    #if len(words) > 10: TODO idk jsut whatever dude this is tiring i want to go home (TRY RECURSIVE FUNC DUMBASS (IT DOESN'T WORK SMARTASS YOU NEED A RULE LIST FIRST))
-    # WHICH MEANS REFACTORINGG THHE WHOLE THING AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-    #    word = words[0].text + words[1].text
-    # TODO : TEXT PADDING FOR /text
     conv = word
     if '\u30a1' <= word[0] <= '\u30f6': # converts katakana to hiragana
         conv = jaconv.kata2hira(word)
-    print(f"WOOOOOOOOOOOORD: {word}")
     for w in [word, conv]:
-        for i in range(len(w)): # first deinflection
-            if i == 0:
-                continue
+        for i in range(1, len(w)): # first deinflection, skips i = 0 to reduce noises
             substring = w[:i+1]
-            for rule in all_rules:
-                #candidates.append((candidate, rule["rulesOut"]))
-                # each loop checks whether the current substring ends with the current deinflection rule 
-                if substring.endswith(rule["kanaIn"]):
-                    candidate = substring[:-len(rule["kanaIn"])] + rule["kanaOut"]
-                    pairs.append((substring, candidate, rule["rulesOut"]))
-                    candidates.append(candidate)
-                    deinflects.append(rule)
-    if candidates: # multi-step deinflection
-        for candidate in candidates:
-            for rule in all_rules:
-                if candidate.endswith(rule["kanaIn"]):
-                    candidate_new = candidate[0][:-len(rule["kanaIn"])] + rule["kanaOut"]
-                    if candidate_new not in [candidate[0] for candidate in candidates]:
-                        pairs.append((candidate, candidate_new, rule["rulesOut"]))
-                        candidates.append(candidate_new)
-                        deinflects.append(rule)
+            print(f"sub: {substring}")
+            # sends substring of incremental len to deinflect
+            # putting this in here for now, might move later
+            for d in deinflect_word(substring): 
+                if d["word"] != substring: # this just ensures word doesn't duplicate with word param in the query later
+                    deinflects.append({
+                        "original" : substring,
+                        "word" : d["word"],
+                        "rules" : d["rules"] 
+                    })
+    candidates = [d["word"] for d in deinflects]
     placeholders = ','.join("?" * len(candidates))
     if dict == "jp":
     # so the way the query below works is that it will get all words from the db one by one 
@@ -86,9 +114,6 @@ def lookup(word: str, dict: str = "en") -> list:    # "en" is the default arg
     # for example does the pattern 民主% match 民主主義？ (or vice versa) 
     # the parentheses at the start are important to wrap the results of the two selects into one, otherwise it will return an error 
     # TODO: FIX DEINFLECTION AND SORTING AND HIGHLIGHING (done?　YES THEY'RE ALL DONE DON'T TOUCH THEM ANYMORE *coping*) 
-    # ) AND (rule IN ({placeholders}))
-    # + [str(candidate[1]) for candidate in candidates]
-        print(f"asd: {word}")
         cursor.execute(f"""
             SELECT * FROM (
                 SELECT *, ? as input FROM jpdict WHERE word IN ({placeholders}) OR reading IN ({placeholders})
@@ -103,7 +128,7 @@ def lookup(word: str, dict: str = "en") -> list:    # "en" is the default arg
                 END DESC,
                 CASE WHEN input LIKE word || '%' OR input LIKE reading || '%' THEN 1 ELSE 0 END DESC,
                 LENGTH(word) DESC,
-                word ASC""", [word]  + candidates + candidates + [word, word, word, conv, conv])  
+                word ASC""", [word] + candidates + candidates + [word, word, word, conv, conv])  
     # in the case of CASE, it would only get words/readings that are already filtered by WHERE
     # the logic itself is similar to WHERE with the matching stuff
     # theoretically i can merge them into 1 query with dict argument being dict name but i'm not risking it 
@@ -120,29 +145,25 @@ def lookup(word: str, dict: str = "en") -> list:    # "en" is the default arg
                 ELSE LENGTH(reading)
             END DESC,
             word ASC""", [word] + candidates + candidates + [word, word, word, conv, conv])  
-    print(f"{word}, {conv}") 
     result = cursor.fetchall()
-    # validates so only deinflected words (pair[1]) that are actually exsist in result get in for sorting
-    # (well basically to make the highlighting non-greedy)
-    valid_pairs = [pair for pair in pairs if pair[1] in [r["word"] for r in result]]
-    #for pair in pairs: 
-    #    for r in result:
-
     results = []
     for r in result:
-        #print(word.startswith(r["reading"]))
-        #if word.startswith(r["reading"]):
-        #    print(r["reading"])
-        #print(f"valid: {valid_pairs}")
-        print(f"candidates: {candidates}, pairs: {pairs}")
-        #print(f"reading: '{r['reading']}', len: {len(r['reading'])}")
-        #print(f"startswith: {word.startswith(r['reading'])}, len check: {len(r['reading']) >= 1}")
-        #print(deinflects)
-        if valid_pairs:
-            # 0 = original, 1 = deinflect
-            # also max returns whatever type you put in (in this case tuple), hence the [0] at the very end
-            # why does this work even without filtering??? nope, it only does when valid is truthy
-            length = len(max(valid_pairs, key=lambda x: len(x[0]))[0]) 
+        # this prevents noises to get appended 例: filtering out 乞い from word param こさ
+        is_direct = (word.startswith(r["word"]) or word.startswith(r["reading"]) or conv.startswith(r["reading"]))
+        # these are for the highlighting
+        # basically finds the longest possible string of valid chars 
+        matching_deinflect = None
+        matching_deinflects = [
+            d for d in deinflects
+            if (r["word"] == d["word"] or r["reading"] == d["word"]) # checks if d exists in result
+            and (not d["rules"] or r["rule"] in d["rules"])  # checks if d has rule or one of its rules matches with r's
+        ]
+        if matching_deinflects:
+            matching_deinflect = max(matching_deinflects, key=lambda x: len(x["original"])) # original refers to the pre-inflected word
+        if not is_direct and not matching_deinflects:
+            continue
+        if matching_deinflect:
+            length = len(matching_deinflect["original"])
         elif word.startswith(r["reading"]) and len(r["reading"]) >= 1:
             length = len(r["reading"])
         elif conv.startswith(r["reading"]) and len(r["reading"]) >= 1:
@@ -152,20 +173,12 @@ def lookup(word: str, dict: str = "en") -> list:    # "en" is the default arg
         # since it's a json string by default i need to parse the def back as a python object
         # well it's because i didn't set ensure_ascii to True when dumping them into the database  
         # which turned the letters into unicode bytes 
-        for pair in pairs:
-            for rule in pair[2]:
-                cursor.execute("""
-                    SELECT * FROM jpdict
-                    WHERE (word = ? OR reading = ?) AND rule = ?
-                """, [pair[1], pair[1], rule])
-                for r in cursor.fetchall():
-                    results.append({
-                        "word": r["word"],
-                        "reading": r["reading"],
-                        "definition": json.loads(r["definition"]),
-                        "len": length
-                    })
-                                            
+        results.append({
+            "word": r["word"],
+            "reading": r["reading"],
+            "definition": json.loads(r["definition"]),
+            "len": length
+        })          
     conn.close()
     return results                                    
 
